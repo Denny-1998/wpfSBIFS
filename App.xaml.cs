aaad75d64a6aee718bf13eaa63a34ae1449a2775
ChrisOtt0 <sophusott@gmail.com>
2022-11-07 18:09:01 +0100

    Setup View change to be ViewModel first. Project requires Unity to be able to change View.

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using Unity;
+using wpfSBIFS.ViewModel;
 
 namespace wpfSBIFS
 {
@@ -13,5 +16,21 @@ namespace wpfSBIFS
     /// </summary>
     public partial class App : Application
     {
+        // UnityContainer - DI for ViewModel first
+        public static IUnityContainer container = new UnityContainer();
+
+        // Reference to main windows content control
+        public ContentControl ccRef { get; set; } = null;
+
+        public App()
+        {
+            container.RegisterType<ILoginViewModel, LoginViewModel>();
+        }
+
+        // Method to change view
+        public void ChangeUserControl(UserControl view)
+        {
+            this.ccRef.Content = view;
+        }
     }
 }
