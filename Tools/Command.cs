de2981de7887d8181362c340532f3545dcf2ce74
ChrisOtt0 <sophusott@gmail.com>
2022-11-07 14:02:54 +0100

    Add project files.

---
+++ Tools/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfSBIFS.Tools
+{
+    internal class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public Action<object> actionToInvoke;
+
+        public bool CanExecute(object? parameter)
+        {
+            return actionToInvoke != null;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (CanExecute(parameter))
+                this.actionToInvoke(parameter);
+        }
+
+        public Command(Action<object> actionToInvoke)
+        {
+            this.actionToInvoke = actionToInvoke;
+        }
+    }
+}
