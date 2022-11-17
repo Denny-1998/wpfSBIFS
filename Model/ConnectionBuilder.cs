bd62396b50084ca716886407dde4004607e3aef2
Denny-1998 <89900734+Denny-1998@users.noreply.github.com>
2022-11-17 17:21:36 +0100

    added connection builder and connection adapter further implementation will happen later

---
+++ Model/ConnectionBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfSBIFS.Model
+{
+    public class ConnectionBuilder
+    {
+        TcpClient tcp;
+
+        string _hostName;
+        int _port;
+
+
+
+        public void AddHostname (string hostName)
+        {
+            _hostName = hostName;
+        }
+
+
+        public void AddPort (int port)
+        {
+            _port = port;
+        }
+
+
+        public async Task<TcpClient> Connect()
+        {
+            //check if both values are present 
+            if (tcp == null) return null;
+            if (_port == 0) _port = 8080;
+
+            //make new connection
+            tcp = new TcpClient (_hostName, _port);
+            return tcp;
+        }
+
+
+    }
+}
---
+++ Model/ServerConnectionAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfSBIFS.Model
+{
+    public class ServerConnectionAdapter
+    {
+        
+        TcpClient _client;
+        NetworkStream _ns;
+        StreamReader _reader;
+
+        string _HostName;
+        int _Port;
+
+
+        public ServerConnectionAdapter(string HostName, int Port)
+        {
+            _HostName = HostName;
+            _Port = Port;
+            
+            connect();
+        }
+
+        private async Task connect()
+        {
+            //build new connection
+            ConnectionBuilder cb = new ConnectionBuilder();
+            cb.AddHostname(_HostName);
+            cb.AddPort(_Port);
+
+            //connect 
+            _client = await cb.Connect();
+        }
+
+
+
+        public async Task Login(string User, string Password)
+        {
+            //do stuff
+        }
+
+    }
+}
